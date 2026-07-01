c9645a388f425b38aaec3e479ca96b0e23c527d0
BHAL BHEEM METHREE <bhalbheemmethree@gmail.com>
2026-07-01 11:49:43 +0530

    escape sequence and comments

--- day02.py
+++ day02.py
@@ -1,7 +1,11 @@
-# comments, Escape Sequences and print statements
+# comments, Escape Sequences and print 
+  •escape sequence
+  °comments 
 
+•
 print("Hey I am a good boy and this\n"
       "viewer is also a good boy")
 
+•
 # by using \n we can print in new line
 # by using ctrl + / we can comment multiple lines at a time
