8435f5979d913049254264756585ce5ad127eacb
bhalbheemmethree-star <bhalbheemmethree@gmail.com>
2026-07-03 01:30:10 +0530

    while loops in python

--- day12.py
+++ day12.py
@@ -1,1 +1,28 @@
 # while loops in python 
+
+i = 0
+while(i<=3):
+    print(i)
+    i += 1
+
+
+i = int(input("enter a number:"))
+print(i)
+while(i<=20):
+    i = int(input("enter a number:"))
+    print(i)
+
+print("Done with the loop")
+
+# decreamenting while loop
+
+count = 5
+while(count>0):
+    print(count)
+    count 
+    count -= 1
+
+
+
+
+    
